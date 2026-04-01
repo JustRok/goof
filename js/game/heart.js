96fbd041b60aaf4c606da5c2bb409084d3faa5b2
Rok <toetickler12itookoffyourshoe@proton.me>
2026-04-01 15:39:28 +0000

    make hp look goober more

--- js/game/heart.js
+++ js/game/heart.js
@@ -190,7 +190,7 @@ Heart.prototype.render = function() {
 	this.graphics.clear();
 	this.graphics.beginFill(0xff0000);
 	this.graphics.lineStyle(0, 0xFFFFFF);
-	this.graphics.drawRect(262, 447, 28, 21);
+	this.graphics.drawRect(142, 447, 28, 21);
 	this.graphics.beginFill(0xffff00);
 	this.graphics.drawRect(262, 447, this.hp * 28 / this.maxhp, 21);
 	this.graphics.endFill();
